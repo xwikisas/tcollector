72c837ab65d9c384c86129d9d4fa78672e52dc66
Daniel Patriche <rebelthor@gmail.com>
2014-02-21 11:40:34 +0100

    Add NTP offset collector

---
+++ collectors/0/ntpoffset.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import time
+import sys
+from collectors.lib import ntplib
+
+COLLECTION_INTERVAL = 15  # seconds
+NTP_SERVER = 'ntp.ovh.net'
+
+def main():
+  client = ntplib.NTPClient()
+  while True:
+     ts = int(time.time())
+
+     response = client.request(NTP_SERVER, version=3)
+     print ("sys.time.ntp_offset %d %.5f"
+        % (ts,response.offset))
+
+     sys.stdout.flush()
+     time.sleep(COLLECTION_INTERVAL)
+
+if __name__ == "__main__":
+  sys.exit(main())
+
+
