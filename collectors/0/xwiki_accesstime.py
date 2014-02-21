55dede410af203a5f516753031418cd5e0a73959
Daniel Patriche <rebelthor@gmail.com>
2014-02-21 11:27:23 +0100

    Better handle empty/inconfigured XInit URL

--- collectors/0/xwiki_accesstime.py
+++ collectors/0/xwiki_accesstime.py
@@ -2,23 +2,34 @@
 
 import sys
 import time
+import os.path
 from subprocess import check_output as qx
 
 COLLECTION_INTERVAL = 300  # seconds
-URL = qx(["-c", "grep CHECK_HTTP_URL= /etc/xinit/xinit.cfg | sed 's/CHECK_HTTP_URL=\"//' | sed 's/\"//'" ], shell=True)
+xinit_config_file = '/etc/xinit/xinit.cfg'
+
+if not os.path.isfile(xinit_config_file):
+  sys.exit(13)
+
+URL = qx(["-c", "grep CHECK_HTTP_URL= "+xinit_config_file+" | sed 's/CHECK_HTTP_URL=\"//' | sed 's/\"//'" ], shell=True).rstrip()
+
+if URL == '' or URL == '#':
+  sys.exit(13)
+
 def main():
   while True:
-     ts = int(time.time())
-     values = qx(["-c", "curl -o /dev/null --silent " + URL.rstrip() + " -w %{time_connect}:%{time_starttransfer}:%{time_total}"], shell=True)
-     svalues= values.split(':')
-     print ("xwiki.accesstime.time_connect %d %s"
+    ts = int(time.time())
+    values = qx(["-c", "curl -o /dev/null --silent " + URL.rstrip() + " -w %{time_connect}:%{time_starttransfer}:%{time_total}"], shell=True)
+    svalues= values.split(':')
+    print ("xwiki.accesstime.time_connect %d %s"
         % (ts,int(float(svalues[0])*1000)))
-     print ("xwiki.accesstime.time_transfer %d %s"
+    print ("xwiki.accesstime.time_transfer %d %s"
         % (ts,int(float(svalues[1])*1000)))
-     print ("xwiki.accesstime.time_total %d %s"
+    print ("xwiki.accesstime.time_total %d %s"
         % (ts,int(float(svalues[2])*1000)))
-     sys.stdout.flush()
-     time.sleep(COLLECTION_INTERVAL)
+    sys.stdout.flush()
+    time.sleep(COLLECTION_INTERVAL)
+
 
 if __name__ == "__main__":
   sys.exit(main())
